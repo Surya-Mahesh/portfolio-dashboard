4b78ce5a66b79dc4a86538602618c4a1d98a32d7
Sonti Venkata Surya Mahesh <suryamaheshsonti@gmail.com>
2025-08-24 23:47:49 +0530

    Done with the 1st step

---
+++ src/projects/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-green-400">
+      <h1 className="text-5xl font-bold animate-pulse">
+        🚀 Tailwind is Working!
+      </h1>
+    </div>
+  )
+}
